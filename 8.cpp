d95d99f6a0fee957d9a084edbbd22d9c89c5aa5b
kiranloka <112564858+kiranloka@users.noreply.github.com>
2023-03-29 00:24:06 +0530

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main() 
+{ 
+   foo();
+}  
+int foo() 
+{ 
+   printf("Hello"); 
+   return 0;  
+}
+
+
+// the program doesnt run in c++ cause foo is not declared and printf is not the function to get output in c++
