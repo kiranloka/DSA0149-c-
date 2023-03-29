e845847dc9d99e71459dc4157d87a602daa264e2
kiranloka <112564858+kiranloka@users.noreply.github.com>
2023-03-29 11:35:45 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main() {
 int a ;
 cin>>a;
-if(a%2 == 0)  // if remainder is zero then even number
+if(a%2 == 0) { // if remainder is zero then even number
     cout<<”even”;
-else       cout<<”odd”;
+}
+else      {
+    cout<<”odd”;
+}
 return 0;
 }
